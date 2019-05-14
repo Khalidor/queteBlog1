b3fd39d15b6bb8827d07d39b26677ddedd24ff8b
khalidor <elghadir.khalid@gmail.com>
2019-05-14 17:47:36 +0200

    avec le vendor

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 class BlogController extends AbstractController
 {
+    /**
+    * @Route("/blog", name="blog_index")
+    */
+
     public function index()
     {
-        return new Response(
-            '<html><body>Blog Index</body></html>'
-        );
+        return $this->render('blog/index.html.twig', [
+            'owner' => 'Thomas',
+        ]);
     }
 }
